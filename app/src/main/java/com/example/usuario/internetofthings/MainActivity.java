c629a5fa597e2ba1a1cef27a5b9380eaac25b503
Fernando <fernando.cardenasm@gmail.com>
2015-06-07 17:29:01 +0200

    Add new Adapter LeDeviceListAdapter

--- app/src/main/java/com/example/usuario/internetofthings/MainActivity.java
+++ app/src/main/java/com/example/usuario/internetofthings/MainActivity.java
@@ -1,18 +1,34 @@
 package com.example.usuario.internetofthings;
 
+import android.app.Activity;
 import android.app.ListActivity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Handler;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends ListActivity {
 
     // Initializes Bluetooth adapter.
-//    final BluetoothManager bluetoothManager =
-//            (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
-//    private BluetoothAdapter mBluetoothAdapter;
 
+    private BluetoothAdapter mBluetoothAdapter;
+    private boolean mScanning;
+    private Handler mHandler;
+    private LeDeviceListAdapter mLeDeviceListAdapter;
+
+
+    private static final int REQUEST_ENABLE_BT = 1;
+    // Stops scanning after 10 seconds.
+    private static final long SCAN_PERIOD = 10000;
+
+    ArrayList<BluetoothDevice> mLeDevices;
     private BTLE[] mBTLEDevices;
     int numDevices = 2;
 
@@ -21,39 +37,78 @@ public class MainActivity extends ListActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mBTLEDevices = new BTLE[numDevices];
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+        mLeDevices = new ArrayList<BluetoothDevice>();
+        mHandler = new Handler();
 
-        for(int i = 0; i < numDevices; i++){
-            BTLE device = new BTLE();
-            device.setFriendlyName("AAA");
-            device.setDeviceAddress("MAC100");
-            mBTLEDevices[i] = device;
+        // Checks if Bluetooth is supported on the device.
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this, R.string.error_bluetooth_not_supported, Toast.LENGTH_SHORT).show();
+            finish();
+            return;
         }
 
-        BTLEAdapter btleAdapter = new BTLEAdapter(this, mBTLEDevices);
-        setListAdapter(btleAdapter);
+//        mBTLEDevices = new BTLE[numDevices];
+//
+//        for(int i = 0; i < numDevices; i++){
+//            BTLE device = new BTLE();
+//            device.setFriendlyName("AAA");
+//            device.setDeviceAddress("MAC100");
+//            mBTLEDevices[i] = device;
+//        }
+//
+//        BTLEAdapter btleAdapter = new BTLEAdapter(this, mBTLEDevices);
+//        setListAdapter(btleAdapter);
     }
 
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    protected void onResume() {
+        super.onResume();
+        // Ensures Bluetooth is enabled on the device.  If Bluetooth is not currently enabled,
+        // fire an intent to display a dialog asking the user to grant permission to enable it.
+        if (!mBluetoothAdapter.isEnabled()) {
+            if (!mBluetoothAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
         }
 
-        return super.onOptionsItemSelected(item);
+        // Initializes list view adapter.
+
+        mLeDeviceListAdapter = new LeDeviceListAdapter(this, mLeDevices);
+        setListAdapter(mLeDeviceListAdapter);
+        Toast.makeText(this, "Probando", Toast.LENGTH_SHORT).show();
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // User chose not to enable Bluetooth.
+        if (requestCode == REQUEST_ENABLE_BT && resultCode == Activity.RESULT_CANCELED) {
+            finish();
+            return;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //scanLeDevice(false);
+        clear();
+    }
+
+    public void addDevice(BluetoothDevice device) {
+        if(!mLeDevices.contains(device)) {
+            mLeDevices.add(device);
+        }
+    }
+    public BluetoothDevice getDevice(int position) {
+        return mLeDevices.get(position);
+    }
+    public void clear() {
+        mLeDevices.clear();
+    }
+
 }
